7c9c183cf49c45582056576049da69f0d8c2a64e
shaelsien630 <shaelsien630@gmail.com>
2024-11-19 19:57:13 +0900

    Initial Commit

---
+++ Korbit/KorbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KorbitApp.swift
+//  Korbit
+//
+//  Created by 최서희 on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct KorbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
